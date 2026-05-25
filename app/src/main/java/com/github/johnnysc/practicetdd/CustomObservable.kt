1d8df6af0268c875224bfb002fb82695be6b4652
sergei <@email>
2026-05-25 08:56:26 +0300

    observer impl

---
+++ app/src/main/java/com/github/johnnysc/practicetdd/CustomObservable.kt
@@ -0,0 +1,27 @@
+package com.github.johnnysc.practicetdd
+
+interface CustomObservable<T, S> {
+
+    fun addObserver(observer: S)
+
+    fun update(argument: T)
+
+    fun removeObserver(observer: S)
+
+    class Base<T, S : CustomObserver<T>> : CustomObservable<T, S> {
+
+        private val observers = mutableListOf<S>()
+
+        override fun addObserver(observer: S) {
+            observers.add(observer)
+        }
+
+        override fun update(argument: T) {
+            observers.forEach { it.update(argument) }
+        }
+
+        override fun removeObserver(observer: S) {
+            observers.remove(observer)
+        }
+    }
+}
